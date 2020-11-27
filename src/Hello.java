fadeb47737ecec3e9c2f5de174f6c8467990c8e0
yassineuuu <yassinemakhlouk49@gmail.com>
2020-11-27 10:51:39 +0000

    firstcommit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+        String msg = "Hello World";
+
+        System.out.println(msg);
+
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+
+        boolean d = c < b;
+        boolean e = c > a;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
